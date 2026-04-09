cfa657b274560bd98b58629ee57e0fdb29d681d4
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-04-09 12:16:50 -0500

    Creacion Migración TablaEvaluaciones

--- database/migrations/2026_04_09_161706_crear_tabla_evaluaciones.php
+++ database/migrations/2026_04_09_161706_crear_tabla_evaluaciones.php
@@ -22,6 +22,7 @@ return new class extends Migration {
             $table->date('fechafin');
             $table->integer('intentospermitidos');
             $table->boolean('activo');
+            $table->timestamps();
         });
     }
 
