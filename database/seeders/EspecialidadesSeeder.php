68b9ed62aab8890123eb09806694bbbc031f64ce
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-04-09 11:55:07 -0500

    Creacion Semilla Especialidades

--- database/seeders/EspecialidadesSeeder.php
+++ database/seeders/EspecialidadesSeeder.php
@@ -3,15 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class EspecialidadesSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         DB::table('especialidades')->insert([
