2eea0a103a21a54d55a4ad9a531cebc21251d704
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-04-09 11:03:47 -0500

    Creacion Migración TablaMetodosCursos

---
+++ database/migrations/2026_04_09_160016_crear_tabla_cursos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cursos', function (blueprint $table) {
+            $table->id('idcurso');
+            $table->foreignId('idespecialidad')->constrained('especialidades');
+            $table->string('pathbanner')->nullable();
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->integer('cantidadhoras');
+            $table->decimal('precioreferencial', 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cursos');
+    }
+};
