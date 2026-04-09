77d912ae36b2709d3c0a73575c088e2726b4483a
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-04-09 11:11:43 -0500

    Creacion Migración TablaHorarios

--- database/migrations/2026_04_09_160541_crear_tabla_horarios.php
+++ database/migrations/2026_04_09_160541_crear_tabla_horarios.php
@@ -24,6 +24,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
+        // Drop de la tabla 'horarios' si existe
         Schema::dropIfExists('horarios');
     }
 };
