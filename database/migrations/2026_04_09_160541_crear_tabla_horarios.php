7c3cc171c0293640fc2c91dd8cc0edcbf277fa38
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-04-09 11:31:26 -0500

    Creacion Migración TablaHorarios

--- database/migrations/2026_04_09_160541_crear_tabla_horarios.php
+++ database/migrations/2026_04_09_160541_crear_tabla_horarios.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('horarios', function (Blueprint $table) {
             $table->id('idhorario');
-            $table->foreignId('idcapacitacion')->constrained('capacitaciones');
+            $table->foreignId('idcapacitacion')->constrained('capacitaciones', 'idcapacitacion');
             $table->date('fecha');
             $table->time('inicio');
             $table->time('fin');
