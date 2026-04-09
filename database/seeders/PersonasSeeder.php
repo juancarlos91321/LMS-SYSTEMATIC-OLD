a89dd2a9365d94aac8bb8497d09661c23665af08
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-04-09 11:54:50 -0500

    Creacion Semilla Personas

--- database/seeders/PersonasSeeder.php
+++ database/seeders/PersonasSeeder.php
@@ -3,15 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class PersonasSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         DB::table('personas')->insert([
